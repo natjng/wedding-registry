a432b8eac79af8a547eae7bdd2479f72cc8a134f
Natalie <natsetc@gmail.com>
2019-07-10 14:21:47 -0700

    create routes for login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
+    get '/users' do 
+        @user = User.find(1)
+        # need to change later
+        erb :'users/show'
+    end
 
+    get '/users/signup' do 
+        erb :'users/signup'
+    end
+
+    get '/users/login' do 
+        erb :'users/login'
+    end
 end
