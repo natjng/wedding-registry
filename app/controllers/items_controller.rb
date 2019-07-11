bb248ae7b0387d0df0441e7ca055df6c4b94fcd8
Natalie <natsetc@gmail.com>
2019-07-10 23:43:55 -0700

    create index route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,9 @@
 class ItemsController < ApplicationController
-
+    get '/items' do 
+        @user = User.find(2)
+        # change to User.find(session[:user_id])
+        @items = Item.all
+        erb :'items/index'
+        # show all users items
+    end
 end
