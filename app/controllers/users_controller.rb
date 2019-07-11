abf3a6d63175491a645a5dacfb49950a55755223
Natalie <natsetc@gmail.com>
2019-07-10 23:43:41 -0700

    comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
     get '/users' do 
         @user = User.find(1)
-        # need to change later
+        # change to User.find(session[:user_id])
         erb :'users/show'
+        # need to change view later 'users/:id'
     end
 
     get '/users/signup' do 
