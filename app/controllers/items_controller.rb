945cb68919db003c4049cb59a6aa8b97a4999c8d
Natalie <natsetc@gmail.com>
2019-07-12 03:06:12 -0700

    edit items index to show logged in users items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
     get '/items' do 
-        @user = User.find(2)
-        # change to User.find(session[:user_id])
+        @user = User.find(session[:user_id])
         @items = Item.all
         erb :'items/index'
         # show all users items
