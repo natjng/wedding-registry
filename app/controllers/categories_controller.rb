b7c6202a63599998c9166b165f320d36a7164b37
Natalie <natsetc@gmail.com>
2019-07-11 18:02:14 -0700

    add @user to categories index route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
     get '/categories' do 
         @categories = Category.all
+        @user = User.find(3)
         erb :'categories/index'
+
+        # edit to show categories for users items in their registry
     end
 end
