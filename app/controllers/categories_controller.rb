045aa6ddb19e401c96ee79e932bf907728a1d569
Natalie <natsetc@gmail.com>
2019-07-12 18:39:24 -0700

    edit to show categories for users

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,8 @@
 class CategoriesController < ApplicationController
     get '/categories' do 
         @categories = Category.all
-        @user = User.find(3)
+        @user = User.find(session[:user_id])
         erb :'categories/index'
-
-        # edit to show categories for users items in their registry
     end
 
     get '/categories/new' do 
