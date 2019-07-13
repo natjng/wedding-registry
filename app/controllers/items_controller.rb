21801973719a84eae41923e8a7dfeab616f1ab60
Natalie <natsetc@gmail.com>
2019-07-12 20:29:08 -0700

    debug categories association for items post route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,25 +12,18 @@ class ItemsController < ApplicationController
     end
 
     post '/items' do 
-        # params[:name] != ""
-        if !params[:name].empty? && !params[:category_id].empty?
+        @user = User.find(session[:user_id])
+
+        if !params[:name].empty? && !!params[:category_id]
             @item = Item.create(name: params[:name], category_id: params[:category_id])
-            @user = User.find(session[:user_id])
             @user.items << @item
-
             redirect "/items/#{@item.id}"
-
-            # @item = Item.create(name: params[:name], category_id: params[:category_id], user_id: params[:user_id])
-            # will user know about item this way?
-
-        elsif !params[:name].empty? && !params["category_name"].empty?
+        elsif !params[:name].empty? && params["category_name"] != ""
             @item = Item.create(name: params[:name])
-            @user = User.find(session[:user_id])
             @user.items << @item
-
-            category = Category.create(params["category_name"])
-            category.items << @item
-
+            @category = Category.find_or_create_by(name: params[:category_name])
+            # use find_or_create_by so user does not create duplicate categories
+            @category.items << @item
             redirect "/items/#{@item.id}"
         else
             redirect '/items/new'
